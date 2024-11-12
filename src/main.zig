6b2eaa375ec1acc4b91189540b91d4846545e3ae
Davy <d.vdschoot@gmail.com>
2024-11-12 13:05:04 +0100

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,98 +1,38 @@
 const std = @import("std");
+const stderr = std.io.getStdErr().writer();
+const stdout = std.io.getStdOut().writer();
 
 const MyErrors = error{tokenNotFound};
-const TokenType = enum {
-    EOF,
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
-    STAR,
-    DOT,
-    COMMA,
-    PLUS,
-    MINUS,
-    SEMICOLON,
+const TokenType = enum(u8) {
+    LEFT_PAREN = '(',
+    RIGHT_PAREN = ')',
+    LEFT_BRACE = '{',
+    RIGHT_BRACE = '}',
+    COMMA = ',',
+    DOT = '.',
+    PLUS = '+',
+    MINUS = '-',
+    LESS = '<',
+    GREATER = '>',
+    SLASH = '/',
+    SEMICOLON = ';',
+    STAR = '*',
+    EOF = 0,
 };
-
-const Token = struct {
-    tokenType: TokenType,
-    lexeme: []const u8,
-    literal: ?[]u8,
-};
-
-const LParenToken = Token{
-    .tokenType = .LEFT_PAREN,
-    .lexeme = "(",
-    .literal = null,
-};
-
-const RParenToken = Token{
-    .tokenType = .RIGHT_PAREN,
-    .lexeme = ")",
-    .literal = null,
-};
-
-const EOFToken = Token{
-    .tokenType = .EOF,
-    .lexeme = "",
-    .literal = null,
-};
-
-fn match(c: u8) MyErrors!Token {
-    switch (c) {
-        '(' => {
-            return LParenToken;
-        },
-        ')' => {
-            return RParenToken;
-        },
-        '{' => {
-            return Token{ .tokenType = .LEFT_BRACE, .lexeme = "{", .literal = null };
-        },
-        '}' => {
-            return Token{ .tokenType = .RIGHT_BRACE, .lexeme = "}", .literal = null };
-        },
-        ',' => {
-            return Token{ .tokenType = .COMMA, .lexeme = ",", .literal = null };
-        },
-        '.' => {
-            return Token{ .tokenType = .DOT, .lexeme = ".", .literal = null };
-        },
-        '-' => {
-            return Token{ .tokenType = .MINUS, .lexeme = "-", .literal = null };
-        },
-        '+' => {
-            return Token{ .tokenType = .PLUS, .lexeme = "+", .literal = null };
-        },
-        ';' => {
-            return Token{ .tokenType = .SEMICOLON, .lexeme = ";", .literal = null };
-        },
-        '*' => {
-            return Token{ .tokenType = .STAR, .lexeme = "*", .literal = null };
-        },
-        0 => {
-            return EOFToken;
-        },
-        else => {
-            return MyErrors.tokenNotFound;
-        },
-    }
-}
 
 pub fn main() !void {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std.debug.print("Logs from your program will appear here!\n", .{});
-
     const args = try std.process.argsAlloc(std.heap.page_allocator);
+
     defer std.process.argsFree(std.heap.page_allocator, args);
 
     if (args.len < 3) {
         std.debug.print("Usage: ./your_program.sh tokenize <filename>\n", .{});
+
         std.process.exit(1);
     }
 
     const command = args[1];
+
     const filename = args[2];
 
     if (!std.mem.eql(u8, command, "tokenize")) {
@@ -101,21 +41,83 @@ pub fn main() !void {
     }
 
     const file_contents = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, filename, std.math.maxInt(usize));
+
     defer std.heap.page_allocator.free(file_contents);
 
-    // Uncomment this block to pass the first stage
+    var exit_code: u8 = 0;
+
     if (file_contents.len > 0) {
-        for (file_contents) |c| {
-            if (c == '\n') {
-                std.debug.print("fml", .{});
-                continue;
+        var it = std.mem.tokenizeScalar(u8, file_contents, '\n');
+
+        var line_count: usize = 0;
+
+        while (it.next()) |line| {
+            line_count += 1;
+
+            for (0..line.len) |i| {
+                switch (line[i]) {
+                    '(' => {
+                        try addToken(TokenType.LEFT_PAREN);
+                    },
+                    ')' => {
+                        try addToken(TokenType.RIGHT_PAREN);
+                    },
+                    '{' => {
+                        try addToken(TokenType.LEFT_BRACE);
+                    },
+                    '}' => {
+                        try addToken(TokenType.RIGHT_BRACE);
+                    },
+                    ',' => {
+                        try addToken(TokenType.COMMA);
+                    },
+                    '.' => {
+                        try addToken(TokenType.DOT);
+                    },
+                    '+' => {
+                        try addToken(TokenType.PLUS);
+                    },
+                    '-' => {
+                        try addToken(TokenType.MINUS);
+                    },
+                    '<' => {
+                        try addToken(TokenType.LESS);
+                    },
+                    '>' => {
+                        try addToken(TokenType.GREATER);
+                    },
+                    '/' => {
+                        try addToken(TokenType.SLASH);
+                    },
+                    ';' => {
+                        try addToken(TokenType.SEMICOLON);
+                    },
+                    '*' => {
+                        try addToken(TokenType.STAR);
+                    },
+                    else => {
+                        try stderr.print("[line {d}] Error: Unexpected character: {c}\n", .{ line_count, line[i] });
+                        exit_code = 65;
+                    },
+                }
             }
-            const token = try match(c);
-            try std.io.getStdOut().writer().print("{s} {s} {any}\n", .{ @tagName(token.tokenType), token.lexeme, token.literal });
         }
-        try std.io.getStdOut().writer().print("{s} {s} {any}\n", .{ @tagName(EOFToken.tokenType), EOFToken.lexeme, EOFToken.literal });
+
+        try stdout.print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
+
     } else {
-        // try std.io.getStdOut().writer().print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
-        try std.io.getStdOut().writer().print("{s} {s} {any}\n", .{ @tagName(EOFToken.tokenType), EOFToken.lexeme, EOFToken.literal });
+        try stdout.print("EOF  null\n", .{}); // Placeholder, remove this line when implementing the scanner
+
     }
+    std.process.exit(exit_code);
+}
+
+fn addToken(token: TokenType) !void {
+    try stdout.print("{s} {c} null\n", .{ @tagName(token), @intFromEnum(token) });
+}
+
+fn match(c: u8, i: usize, line: []const u8) bool {
+    const next = i + 1;
+
+    return next < line.len and line[next] == c;
 }
